35e80483f6686b4a26c552767d44354c71810be2
Brian McKenna <brian@simpleenergy.com>
2014-05-07 09:37:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Module Documentation
+
+## Module Control.Process
+
+### Types
+
+    type Channel e a b = Source e (a -> Eff e b)
+
+    data Process f a where
+      Halt :: Process f a
+      Emit :: a -> Process f a -> Process f a
+      Await :: forall s. (forall r. f r -> (r -> Process f a) -> Process f a -> s) -> s -> Process f a
+
+    type Sink e a = Source e (a -> Eff e {  })
+
+    type Source e a = Process (Eff e) a
+
+    data Unit  where
+      Unit :: {  } -> Unit 
+
+
+### Type Class Instances
+
+    instance processApplicative :: Applicative (Process f)
+
+    instance processApply :: Apply (Process f)
+
+    instance processBind :: Bind (Process f)
+
+    instance processFunctor :: Functor (Process f)
+
+    instance processMonad :: Monad (Process f)
+
+    instance processMonoid :: Monoid (Process k a)
+
+    instance processSemigroup :: Semigroup (Process k a)
+
+    instance unitMonoid :: Monoid Unit
+
+    instance unitSemigroup :: Semigroup Unit
+
+
+### Values
+
+    await :: forall f r a. f r -> (r -> Process f a) -> Process f a
+
+    emit :: forall f a. a -> Process f a
+
+    eval :: forall f a. f a -> Process f a
+
+    evalMap :: forall f a b. (a -> f b) -> Process f a -> Process f b
+
+    flatten :: forall f a. Process f (f a) -> Process f a
+
+    run :: forall f a. (Monad f) => Process f a -> f {  }
+
+    runFoldMap :: forall f a b. (Monad f, Monoid b) => (a -> b) -> Process f a -> f b
+
+    runLog :: forall f a. (Monad f) => Process f a -> f [a]
+
+    translate :: forall f g a. (forall b. f b -> g b) -> Process f a -> Process g a
---
+++ src/Control/Process.purs
@@ -0,0 +1,83 @@
+module Control.Process where
+
+  import Control.Monad.Eff
+  import Data.Monoid
+
+  data Process f a = Halt
+                   | Emit a (Process f a)
+                   | Await (forall s. (forall r. f r -> (r -> Process f a) -> Process f a -> s) -> s)
+
+  instance processSemigroup :: Semigroup (Process k a) where
+    (<>) Halt p = p
+    (<>) (Emit h t) p2 = Emit h (t <> p2)
+    (<>) (Await f) p2 = Await (\g -> f (\req recv fb -> g req (\res -> recv res <> p2) (fb <> p2)))
+
+  instance processMonoid :: Monoid (Process k a) where
+    mempty = Halt
+
+  instance processFunctor :: Functor (Process f) where
+    (<$>) f Halt = Halt
+    (<$>) f (Emit h t) = Emit (f h) (f <$> t)
+    (<$>) f (Await g) = Await (\h -> g (\req recv fb -> h req (\res -> f <$> recv res) (f <$> fb)))
+
+  instance processApply :: Apply (Process f) where
+    (<*>) Halt b = Halt
+    (<*>) (Emit h t) b = (h <$> b) <> (t <*> b)
+    (<*>) (Await g) b = Await (\h -> g (\req recv fb -> h req (\res -> recv res <*> b) (fb <*> b)))
+
+  instance processApplicative :: Applicative (Process f) where
+    pure a = Emit a Halt
+
+  instance processBind :: Bind (Process f) where
+    (>>=) Halt f = Halt
+    (>>=) (Emit h t) f = f h <> (t >>= f)
+    (>>=) (Await g) f = Await (\h -> g (\req recv fb -> h req (\res -> recv res >>= f) (fb >>= f)))
+
+  instance processMonad :: Monad (Process f) where
+
+  type Source e a = Process (Eff e) a
+  type Sink e a = Source e (a -> Eff e {})
+  type Channel e a b = Source e (a -> Eff e b)
+
+  await :: forall f r a. f r -> (r -> Process f a) -> Process f a
+  await req recv = Await (\f -> f req recv Halt)
+
+  emit :: forall f a. a -> Process f a
+  emit h = Emit h Halt
+
+  eval :: forall f a. f a -> Process f a
+  eval h = await h emit
+
+  translate :: forall f g a. (forall b. f b -> g b) -> Process f a -> Process g a
+  translate _ Halt = Halt
+  translate f (Emit h t) = Emit h (translate f t)
+  translate f (Await g) = Await (\h -> g (\req recv fb -> h (f req) (\b -> translate f (recv b)) (translate f fb)))
+
+  flatten :: forall f a. Process f (f a) -> Process f a
+  flatten Halt = Halt
+  flatten (Emit h t) = Await (\f -> f h (\a -> Emit a (flatten t)) Halt)
+  flatten (Await f) = Await (\g -> f (\req recv fb -> g req (\a -> flatten (recv a)) (flatten fb)))
+
+  evalMap :: forall f a b. (a -> f b) -> Process f a -> Process f b
+  evalMap f p = flatten (f <$> p)
+
+  runFoldMap :: forall f a b. (Monad f, Monoid b) => (a -> b) -> Process f a -> f b
+  runFoldMap f p = go p mempty
+    where go Halt acc = pure acc
+          go (Emit h t) acc = go t (acc <> f h)
+          go (Await f) acc = f (\req recv fb -> req >>= \s -> go (recv s) acc)
+
+  runLog :: forall f a. (Monad f) => Process f a -> f [a]
+  runLog = runFoldMap (\x -> [x])
+
+  -- Isomorphic to {}
+  data Unit = Unit {}
+
+  instance unitSemigroup :: Semigroup Unit where
+    (<>) (Unit {}) (Unit {}) = Unit {}
+
+  instance unitMonoid :: Monoid Unit where
+    mempty = Unit {}
+
+  run :: forall f a. (Monad f) => Process f a -> f {}
+  run p = const {} <$> runFoldMap (const (Unit {})) p
